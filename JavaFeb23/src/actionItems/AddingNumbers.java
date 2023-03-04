5e7845b6fb3ad1c2b11119aa7b4f9a505cbe009f
sazzad3139bd <sazzad3139@gmail.com>
2023-03-04 22:49:54 +0600

    Added.........

---
+++ JavaFeb23/src/actionItems/AddingNumbers.java
@@ -0,0 +1,14 @@
+package actionItems;
+
+public class AddingNumbers {
+
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		for (int i = 0; i<=15; i++) {
+			sum = sum +i;
+		}
+		System.out.println("The result is : " + sum);
+	}
+
+}
